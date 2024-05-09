84a13d91a823e2d014188a73b1cf7b23ed07b52b
hendry <h3ndry@users.noreply.github.com>
2024-05-09 09:49:38 +0200

    style(nav-bar): add nav bar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HttpClientModule, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HttpClientModule, CommonModule, NavComponent]
 })
 
 export class AppComponent implements OnInit {
