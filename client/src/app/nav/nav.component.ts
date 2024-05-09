ba5d61d8b54b59eb4d377a00252c7b2b22d03cf9
hendry <h3ndry@users.noreply.github.com>
2024-05-09 14:22:26 +0200

    chore(hustle): hustle boi, one day the sun will shine on you

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
-export class NavComponent {
+
+export class NavComponent implements OnInit {
+
+  model: any = {}
+
+  constructor() {
+    // super();
+
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  login() {
+    console.log(this.model)
+  }
 
 }
